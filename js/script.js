e4bbe5b154ff1ace4eb6c526f6f27e9da6e98e6c
AngeloC89 <Angelociulla89@gmail.com>
2024-03-15 17:31:21 +0100

    i try to add colors for even squares

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const bigBox = document.createElement('div');
 bigBox.className = 'd-flex justify-content-between align-content-center flex-wrap';
 // console.log(square)
 
-
+let button = document.querySelector('.btn btn-primary')
 
 
 for (let i = 1; i <= 64; i++) {
@@ -13,10 +13,25 @@ for (let i = 1; i <= 64; i++) {
 
     const tmpHtml = NewSquare(i)
 
+    tmpHtml.addEventListener('click', function () {
+        if (even(i)) {
+            tmpHtml.classList.add('even')
+        } else {
+            tmpHtml.classList.add('odd')
+        };
+
+
+
+
+
+
+    })
+
+
 
 
     bigBox.appendChild(tmpHtml);
-    // console.log(square);
+    // console.log(bigBox);
     const container = document.querySelector('.container');
     container.append(bigBox);
 }
@@ -24,11 +39,18 @@ for (let i = 1; i <= 64; i++) {
 
 function NewSquare(content) {
     const newSquare = document.createElement('div');
-    newSquare.innerHTML = content; 
+    newSquare.innerHTML = content;
     newSquare.setAttribute('id', 'square');
     return newSquare;
 }
 
+function even(number){
+    if(number % 2 === 0){
+        return 'true';
+    } else{
+        return 'false';
+    }
+}
 
 
 
